e5460f98b0ca343ea3148d8fea430fbc692fffd5
Luis Carlos <luisca_jl@hotmail.com>
2023-06-24 20:36:53 +0200

    Fix change

--- src/main/java/es/lavanda/filebot/parser/repository/FilebotFileRepository.java
+++ src/main/java/es/lavanda/filebot/parser/repository/FilebotFileRepository.java
@@ -1,12 +1,12 @@
 package es.lavanda.filebot.parser.repository;
 
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import es.lavanda.filebot.parser.model.FilebotFile;
 
 
 @Repository
-public interface FilebotFileRepository extends PagingAndSortingRepository<FilebotFile, String> {
+public interface FilebotFileRepository extends MongoRepository<FilebotFile, String> {
     
 }
